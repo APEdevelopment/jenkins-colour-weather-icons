1746ff98fae04898429b06ab552b4a3c17d11d30
APEdevelopment <performanceautomation@outlook.com>
2021-08-20 18:29:29 +0100

    Update and rename theme-colour-weather-icons.js to jenkins-colour-weather-icons.js

--- src/jenkins-colour-weather-icons.js
+++ src/jenkins-colour-weather-icons.js
@@ -1,27 +1,27 @@
 function updateWeatherIcons() {
   for (const svgElement of document.getElementsByClassName("icon-health-80plus")) {
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "#colour-weather-sunny";
+      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/APEdevelopment/jenkins-colour-weather-icons/src/jenkins-colour-weather-icons.css#colour-weather-sunny";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-health-60to79")) {
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "#colour-weather-partly-cloudy";
+      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/APEdevelopment/jenkins-colour-weather-icons/src/jenkins-colour-weather-icons.css#colour-weather-partly-cloudy";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-health-40to59")) {
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "#colour-weather-cloudy";
+      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/APEdevelopment/jenkins-colour-weather-icons/src/jenkins-colour-weather-icons.css#colour-weather-cloudy";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-health-20to39")) {
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "#colour-weather-rainy";
+      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/APEdevelopment/jenkins-colour-weather-icons/src/jenkins-colour-weather-icons.css#colour-weather-rainy";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-health-00to19")) {
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "#colour-weather-pouring";
+      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/APEdevelopment/jenkins-colour-weather-icons/src/jenkins-colour-weather-icons.css#colour-weather-pouring";
     }
   }
 }
