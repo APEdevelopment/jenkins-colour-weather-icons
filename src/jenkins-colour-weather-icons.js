6a35e47628945d724c603a418e58352bcb9f8562
APEdevelopment <performanceautomation@outlook.com>
2021-08-21 11:56:37 +0100

    Update jenkins-colour-weather-icons.js

--- src/jenkins-colour-weather-icons.js
+++ src/jenkins-colour-weather-icons.js
@@ -1,27 +1,29 @@
+var updateWeatherIconsCssLocation = document.currentScript.src.replace(/\.[^/\\.]+$/, ".css")
+
 function updateWeatherIcons() {
   for (const svgElement of document.getElementsByClassName("icon-health-80plus")) {
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/APEdevelopment/jenkins-colour-weather-icons/src/jenkins-colour-weather-icons.css#colour-weather-sunny";
+      useElement.href.baseVal = updateWeatherIconsCssLocation + "#colour-weather-sunny";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-health-60to79")) {
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/APEdevelopment/jenkins-colour-weather-icons/src/jenkins-colour-weather-icons.css#colour-weather-partly-cloudy";
+      useElement.href.baseVal = updateWeatherIconsCssLocation + "#colour-weather-partly-cloudy";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-health-40to59")) {
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/APEdevelopment/jenkins-colour-weather-icons/src/jenkins-colour-weather-icons.css#colour-weather-cloudy";
+      useElement.href.baseVal = updateWeatherIconsCssLocation + "#colour-weather-cloudy";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-health-20to39")) {
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/APEdevelopment/jenkins-colour-weather-icons/src/jenkins-colour-weather-icons.css#colour-weather-rainy";
+      useElement.href.baseVal = updateWeatherIconsCssLocation + "#colour-weather-rainy";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-health-00to19")) {
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/APEdevelopment/jenkins-colour-weather-icons/src/jenkins-colour-weather-icons.css#colour-weather-pouring";
+      useElement.href.baseVal = updateWeatherIconsCssLocation + "#colour-weather-pouring";
     }
   }
 }
